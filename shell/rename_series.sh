e15a28072d7fe3111e5a4d47aed795491412a6e0
Sophie <sophie.homerin@gmail.com>
2013-04-10 18:57:52 -0400

    update renameseries

--- shell/rename_series.sh
+++ shell/rename_series.sh
@@ -1,5 +1,28 @@
-for file in `ls`; do mv $file `echo $file` | sed 's/.*S\([0-9][0-9]\)E\([0-9][0-9]\).*\.\([avi|srt]\)/S\1E\2.\3/'`; done
+#!/bin/bash
+
+#for file in `ls`; do mv $file `echo $file` | sed 's/.*S\([0-9][0-9]\)E\([0-9][0-9]\).*\.\([avi|srt]\)/S\1E\2.\3/'`; done
 
 #pour les noms avec des espaces (mais pas de virgules)
 #todo: ne fait pas le dernier
-m=""; for f in `ls -m *srt`; do if [ -z "$m" ]; then m=$f; else m="$m $f"; fi; if [ `echo $m|grep -c ','` -eq 1 ]; then m=$(echo $m | sed 's/,//'); echo "$m" | sed 's/.*\([0-9]\)x\([0-9]*\).*/0\1E\2.srt/'; m=""; fi; done
+m="";
+
+function rename_file() {
+    m=$(echo "$1" | sed 's/,//');
+    ext=$(echo "$m" | rev | cut -d'.' -f1 | rev);    
+    nm=$(echo "$m" | sed 's/.*\([0-9]\)x\([0-9]*\).*/0\1E\2.'$ext'/');
+    mv "$m" "$nm";
+}
+
+for f in `ls -m`; do
+    if [ -z "$m" ]; then
+        m=$f;
+    else
+        m="$m $f";
+    fi;
+
+    if [ `echo $m|grep -c ','` -eq 1 ]; then
+        rename_file "$m"
+        m="";
+    fi;
+done
+rename_file "$m"
